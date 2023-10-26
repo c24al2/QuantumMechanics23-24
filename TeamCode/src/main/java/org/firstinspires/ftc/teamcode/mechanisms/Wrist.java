e0f5a6e9085790f07237fa801dcab2b21695f499
c24al2 <c24al2@dalton.org>
2023-10-26 18:07:46 -0400

    feat: leveller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Wrist.java
@@ -2,35 +2,36 @@ package org.firstinspires.ftc.teamcode.mechanisms;
 
 import com.qualcomm.robotcore.hardware.Servo;
 public class Wrist {
+    private Servo wrist;
+    private final double flat;
+    private final double three_stack;
+    private final double five_stack;
+    private final double full_flip;
+    public static final double FLAT = 0.4;
+    public static final double THREE_STACK_OFFSET = 0.2;
+    public static final double FIVE_STACK_OFFSET = 0.6;
+    public static final double FULL_FLIP = 0.5;
 
-    private Servo wristServo1;
-    private double defaultOpenPosition1;
-    private double handoffPosition1;
-    private Servo wristServo2;
-    private double defaultOpenPosition2;
-    private double handoffPosition2;
-    public static final double DEFAULT2 = 0.0;
-    public static final double HANDOFF2 = 1.0;
-    public static final double DEFAULT1 = 0.0;
-    public static final double HANDOFF1 = 1.0;
-
-    public Wrist(Servo servo1, Servo servo2) {
-        this.wristServo1 = servo1;
-        this.wristServo2 = servo2;
-        this.defaultOpenPosition1 = DEFAULT1;
-        this.defaultOpenPosition2 = DEFAULT2;
-        this.handoffPosition1 = HANDOFF1;
-        this.handoffPosition2 = HANDOFF2;
+    public Wrist(Servo wrist) {
+        this.wrist = wrist;
+        this.flat = FLAT;
+        this.three_stack = THREE_STACK_OFFSET;
+        this.five_stack = FIVE_STACK_OFFSET;
+        this.full_flip = FULL_FLIP;
     }
 
-    public void open() {
-        wristServo1.setPosition(defaultOpenPosition1);
-        wristServo2.setPosition(defaultOpenPosition2);
+    public void flat_position() {
+        wrist.setPosition(flat);
     }
 
-    public void handoff() {
-        wristServo1.setPosition(handoffPosition1);
-        wristServo2.setPosition(handoffPosition2);
+    public void three_level() {
+        wrist.setPosition(three_stack);
+    }
+    public void five_level() {
+        wrist.setPosition(five_stack);
+    }
+    public void full_flip_level() {
+        wrist.setPosition(full_flip);
     }
 
 }
