45bcbfa1d04abf3b72f3b21c2bf77802a054d4ff
c24al2 <c24al2@dalton.org>
2023-10-26 17:04:13 -0400

    feat: hopper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Hopper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Hopper.java
@@ -3,18 +3,17 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Hopper{
     private Servo hopperServo;
-    private static final double PICKUP_POSITION = 0.2;
-    private static final double DROP_POSITION = 0.8;
+    private static final double PICKUP_1_POSITION = 0.2;
+    private static final double PICKUP_2_POSITION = 0.4;
+    private static final double HANDOFF_POSITION = 0.8;
 
     public Hopper(Servo hopperServo) {
         this.hopperServo = hopperServo;
     }
 
-    public void setPickupPosition() {
-        hopperServo.setPosition(PICKUP_POSITION);
+    public void setPickup1Position() {
+        hopperServo.setPosition(PICKUP_1_POSITION);
     }
-
-    public void setDropPosition() {
-        hopperServo.setPosition(DROP_POSITION);
-    }
+    public void setPickup2Position() {hopperServo.setPosition(PICKUP_2_POSITION);}
+    public void setDropPosition() {hopperServo.setPosition(HANDOFF_POSITION);}
 }
