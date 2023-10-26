aaed433b763ec530e45922b9cde5b6b9321896fb
c24al2 <c24al2@dalton.org>
2023-10-26 17:55:04 -0400

    feat: zOffset

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Flipper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Flipper.java
@@ -3,30 +3,25 @@ package org.firstinspires.ftc.teamcode.mechanisms;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Flipper {
-    private Servo servo1;
-    private Servo servo2;
+    private Servo zOffset;
 
-    public static final double PICKUP_SERVO = 0.0;
-    public static final double DROP_SERVO = 0.5;
-    public static final double VERTICAL_SERVO = 1.0;
+    public static final double FLAT = 0.0;
+    public static final double FIVE_STACK = 0.5;
+    public static final double THREE_STACK = 1.0;
 
-    public Flipper(Servo servo1, Servo servo2) {
-        this.servo1 = servo1;
-        this.servo2 = servo2;
+    public Flipper(Servo zOffset) {
+        this.zOffset = zOffset;
     }
 
-    public void moveToPickup() {
-        servo1.setPosition(PICKUP_SERVO);
-        servo2.setPosition(PICKUP_SERVO);
+    public void Standard_Pickup() {
+        zOffset.setPosition(FLAT);
     }
 
-    public void moveToDrop() {
-        servo1.setPosition(DROP_SERVO);
-        servo2.setPosition(DROP_SERVO);
+    public void Three_Stack() {
+        zOffset.setPosition(THREE_STACK);
     }
 
-    public void moveToVertical() {
-        servo1.setPosition(VERTICAL_SERVO);
-        servo2.setPosition(VERTICAL_SERVO);
+    public void Five_Stack_and_Drop() {
+        zOffset.setPosition(FIVE_STACK);
     }
 }
