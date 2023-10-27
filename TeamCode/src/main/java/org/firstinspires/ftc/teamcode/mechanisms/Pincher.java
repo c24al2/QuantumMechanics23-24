7054450cd4297620cedb3c6920a842072faa89ec
c24al2 <c24al2@dalton.org>
2023-10-27 15:38:16 -0400

    feat: pincher

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Pincher.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Pincher {
+    private Servo pincher;
+
+    public static final double PINCHER_PICKUP = 0.0;
+    public static final double PINCHER_DEPOSIT = 0.5;
+
+    public Pincher(Servo zOffset) {
+        this.pincher = zOffset;
+    }
+
+    public void Pickup_Pincher() {
+        pincher.setPosition(PINCHER_PICKUP);
+    }
+
+    public void Deposit_Pincher() {
+        pincher.setPosition(PINCHER_DEPOSIT);
+    }
+}
