ba3c1a767117bfadc49b2d0ecf031e90823e74ac
c24al2 <c24al2@dalton.org>
2023-10-27 15:32:12 -0400

    feat: pincher wrist

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/PincherWrist.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class PincherWrist {
+    private Servo pincherWrist;
+
+    public static final double PICKUP = 0.0;
+    public static final double DEPOSIT = 0.5;
+    public static final double INTERMEDIATE = 1.0;
+
+    public PincherWrist(Servo zOffset) {
+        this.pincherWrist = zOffset;
+    }
+
+    public void Pickup() {
+        pincherWrist.setPosition(PICKUP);
+    }
+
+    public void Intermediate() {
+        pincherWrist.setPosition(INTERMEDIATE);
+    }
+
+    public void Deposit() {
+        pincherWrist.setPosition(DEPOSIT);
+    }
+}
